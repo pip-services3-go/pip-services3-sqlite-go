152f99bdc6065a19d8962678fe52cbc22f3461ff
nikopol <levichev.dmitry@gmail.com>
2020-12-16 21:07:55 +0300

    Update docs and factory

--- build/DefaultPostgresFactory.go
+++ build/DefaultPostgresFactory.go
@@ -1,28 +1,28 @@
 package build
 
-// import (
-// 	cref "github.com/pip-services3-go/pip-services3-commons-go/refer"
-// 	cbuild "github.com/pip-services3-go/pip-services3-components-go/build"
-// 	ppersist "github.com/pip-services3-go/pip-services3-sqlite-go/persistence"
-// )
+import (
+	cref "github.com/pip-services3-go/pip-services3-commons-go/refer"
+	cbuild "github.com/pip-services3-go/pip-services3-components-go/build"
+	ppersist "github.com/pip-services3-go/pip-services3-sqlite-go/persistence"
+)
 
-// // Creates Sqlite components by their descriptors.
-// // See [[Factory]]
-// // See [[SqliteConnection]]
-// type DefaultSqliteFactory struct {
-// 	cbuild.Factory
-// 	Descriptor                   *cref.Descriptor
-// 	SqliteConnectionDescriptor *cref.Descriptor
-// }
+// Creates Sqlite components by their descriptors.
+// See [[Factory]]
+// See [[SqliteConnection]]
+type DefaultSqliteFactory struct {
+	cbuild.Factory
+	Descriptor                 *cref.Descriptor
+	SqliteConnectionDescriptor *cref.Descriptor
+}
 
-// //	Create a new instance of the factory.
-// func NewDefaultSqliteFactory() *DefaultSqliteFactory {
+//	Create a new instance of the factory.
+func NewDefaultSqliteFactory() *DefaultSqliteFactory {
 
-// 	c := &DefaultSqliteFactory{
+	c := &DefaultSqliteFactory{
 
-// 		Descriptor:                   cref.NewDescriptor("pip-services", "factory", "sqlite", "default", "1.0"),
-// 		SqliteConnectionDescriptor: cref.NewDescriptor("pip-services", "connection", "sqlite", "*", "1.0"),
-// 	}
-// 	c.RegisterType(c.SqliteConnectionDescriptor, ppersist.NewSqliteConnection)
-// 	return c
-// }
+		Descriptor:                 cref.NewDescriptor("pip-services", "factory", "sqlite", "default", "1.0"),
+		SqliteConnectionDescriptor: cref.NewDescriptor("pip-services", "connection", "sqlite", "*", "1.0"),
+	}
+	c.RegisterType(c.SqliteConnectionDescriptor, ppersist.NewSqliteConnection)
+	return c
+}
