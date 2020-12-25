a84f69a52ae1a137d9ab3fbe2c6accb0b91bc033
Dmitriy Krayniy <krdima92@gmail.com>
2020-12-25 12:32:55 +0300

    added github ci

--- build.ps1
+++ build.ps1
@@ -5,19 +5,32 @@ $ErrorActionPreference = "Stop"
 
 # Generate image and container names using the data in the "component.json" file
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-$image="$($component.registry)/$($component.name):$($component.version)-build"
+
+# Get buildnumber from github actions
+if ($env:GITHUB_RUN_NUMBER -ne $null) {
+    $component.build = $env:GITHUB_RUN_NUMBER
+    Set-Content -Path "component.json" -Value $($component | ConvertTo-Json)
+}
+
+$buildImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-build"
 $container=$component.name
 
 # Remove build files
-if (Test-Path "exe/app") {
-    Remove-Item -Recurse -Force -Path "exe/app"
+if (Test-Path "./dist") {
+    $null = Remove-Item -Recurse -Force -Path "./dist/*"
+} else {
+    $null = New-Item -ItemType Directory -Force -Path "./dist"
 }
 
 # Build docker image
-docker build -f docker/Dockerfile.build -t $image .
+docker build -f docker/Dockerfile.build -t $buildImage .
 
-# Create and copy compiled files, then destroy the container
-docker create --name $container $image
-docker cp "$($container):/go/src/app/app" ./exe/app
+# Create and copy compiled files, then destroy
+docker create --name $container $buildImage
+docker cp "$($container):/app/run" ./dist/run
 docker rm $container
 
+if (!(Test-Path "./dist")) {
+    Write-Host "dist folder doesn't exist in root dir. Build failed. Watch logs above."
+    exit 1
+}
